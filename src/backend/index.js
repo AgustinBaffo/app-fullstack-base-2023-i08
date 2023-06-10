6792c259d4d86cf03fda6b1b767c75021c5606e3
AgustinBaffo <abaffo@irobot.com>
2023-06-10 07:06:27 -0300

    list devices from db

--- src/backend/index.js
+++ src/backend/index.js
@@ -24,32 +24,18 @@ app.post('/device/',function(req,res){
     }
     
 });
-app.get('/pepe/', function(req,res) {
-    utils.query("select * from Devices",function(err,rsp,fields){
-        if(err!=null)
-        res.send(JSON.stringify(rsp));
-    });
-  
-});
+
 app.get('/devices/', function(req, res, next) {
-    devices = [
-        { 
-            'id': 1, 
-            'name': 'Lampara 1', 
-            'description': 'Luz living', 
-            'state': 0, 
-            'type': 1, 
-        },
-        { 
-            'id': 2, 
-            'name': 'Ventilador 1', 
-            'description': 'Ventilador Habitacion', 
-            'state': 1, 
-            'type': 2, 
-            
-        },
-    ]
-    res.send(JSON.stringify(devices)).status(200);
+    utils.query("select * from Devices",function(err,rsp,fields){
+        if(err!=null){
+            console.log("error al buscar los datos");
+            res.send("Error al buscar los datos").status(409);
+        }
+        else{
+            console.log("no error");
+            res.send(JSON.stringify(rsp)).status(200);
+        }
+    });
 });
 
 app.listen(PORT, function(req, res) {
