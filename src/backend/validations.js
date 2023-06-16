1d436c23225dfae9fe9ce91fe03f2c20e7091505
AgustinBaffo <abaffo@irobot.com>
2023-06-16 12:19:27 -0300

    fix bug in validation description

--- src/backend/validations.js
+++ src/backend/validations.js
@@ -2,7 +2,7 @@ function validateDeviceName(param) {
     // Debe ser un string no nulo con una longitud menor a 64 y mayor a 0.
     return typeof param === 'string' && param !== null && param.length > 0 && param.length < 64;    
 }
-function validateDeviceDescription(description) {
+function validateDeviceDescription(param) {
     // Debe ser un string no nulo con una longitud menor a 128 y mayor a 0.
     return typeof param === 'string' && param !== null && param.length > 0 && param.length < 128;    
 }
@@ -18,5 +18,7 @@ function validateDeviceState(param) {
 // Exporta las funciones para que puedan ser utilizadas desde otro archivo
 module.exports = {
     validateDeviceName,
-    validateDeviceDescription
+    validateDeviceDescription,
+    validateDeviceType,
+    validateDeviceState
 };
